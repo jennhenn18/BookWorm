277d5346e9cfc5f94fbb505ac3d08a0ae975ea21
Jenn Henderson <jenn.henderson18@gmail.com>
2019-10-01 21:38:02 -0700

    create server file

---
+++ server.js
@@ -0,0 +1,29 @@
+// link dependencies
+var express = require('express');
+
+// setup express app
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// add model folder for data syncing
+var db = require('./models');
+
+// setup data parsing 
+app.use(express.urlencoded({ encoded: true }));
+app.use(express.json());
+
+// add static directory
+app.use(express.static('public'));
+
+// import routes
+require('./routing/books-api-route')(app);
+require('./routing/club-api-routes')(app);
+require('./routing/member-api-routes')(app);
+require('./routing/html-routes')(app);
+
+// syinc our sequelize models and start the express app
+db.sequelize.sync({ force: true}).then(function() {
+    app.listen(PORT, function() {
+        console.log('App listening on PORT ' + PORT);
+    });
+});
