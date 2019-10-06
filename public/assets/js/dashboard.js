e153ad864361a66ab5f316d374ad0c8d9afb5795
Jenn Henderson <jenn.henderson18@gmail.com>
2019-10-05 18:51:02 -0700

    add remove member api

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -1,30 +1,79 @@
 $(function() {
 
-    // Display Members functionality
-    $(document).ready(function(){
-        $.ajax('/api/members/', {
-            method: 'GET',
-        }).then(function(dbMember){
-            for (let i = 0; i < dbMember.length; i++) {
-                const member = dbMember[i].name;
+    // MEMBERS
+    // ===========================================================================================
+        
+        // Display all members
+        $(document).ready(function(){
+            $.ajax('/api/members/', {
+                method: 'GET',
+            }).then(function(dbMember){
+                // loop through each member and do the following...
+                for (let i = 0; i < dbMember.length; i++) {
+                    // store the member name and ID
+                    const memberName = dbMember[i].name;
+                    const memberId = dbMember[i].id;
+
+                    // create a variable to store the list html element
+                    var l = $('<li>');
+                    
+                    // add class to the member name list item
+                    l.addClass(member);
+
+                    // add member name to list item
+                    l.text(memberName);
+
+                    // create variable to create delete button element
+                    var d = $('<button>');
+
+                    // add data type attr to button
+                    d.attr('data-type', memberId)
+                    
+                    // add class to remove button
+                    d.addClass('removeButton')
+
+                    // add remove text to button
+                    d.text('Remove');
+
+                    // append member name and button to list item
+                    $('#member').append(l,d);
+                }
                 console.log(member)
-                $('#member').append('<li>' + member + '</li>')
-            }
+            })
         })
-    })
 
-    // Event functionality
+        // Delete a member
+        $(document).on('click', '.removeButton', function(){
+            var id = $(this).attr('data-type');
+            
+            $.ajax({
+                method: 'DELETE',
+                url: "/api/members/" + id
+            }).then(function(dbMember){
+                // alert('User removed' + dbMember);
+                location.reload();
+            })
+        })
+
+
+
+    // =========================================================================================================
+    
+    
+    // EVENT
     // ========================================
-    $('#eventSubmit').on('click', function(e){
-        e.preventDefault();
 
-        // store the data inputs
-        var location = $('#location').val().trim();
-        var time = $('#time').val().trim();
+        // Add event
+        $('#eventSubmit').on('click', function(e){
+            e.preventDefault();
 
-        // display the event information
-        $('#displayLocation').text(location);
-        $('#displayTime').text(time);
+            // store the data inputs
+            var location = $('#location').val().trim();
+            var time = $('#time').val().trim();
 
-    })
+            // display the event information
+            $('#displayLocation').text(location);
+            $('#displayTime').text(time);
+
+        })
 })
--- routing/member-api-routes.js
+++ routing/member-api-routes.js
@@ -10,33 +10,33 @@ module.exports = function(app) {
         });
     });
 
-// add a member to the book club - this will be triggered by submitting a new member name and email and hitting a submit button
-// ===========================================================================
-// send the book club id and add it to the member table
-// reload page
-// app.post('/api/members/:id, function(req,res) {
-    // db.Member.create({
-        // figure out what is sent in the request so you can capture the proper data here.
-        // name: req.body.name?
-    //}).then(function(dbMember) {
-        // console.log(dbMember)
-        // add member information to the page
-        // reload page
+    // add a member to the book club - this will be triggered by submitting a new member name and email and hitting a submit button
+    // ===========================================================================
+    // send the book club id and add it to the member table
+    // reload page
+    // app.post('/api/members/:id, function(req,res) {
+        // db.Member.create({
+            // figure out what is sent in the request so you can capture the proper data here.
+            // name: req.body.name?
+        //}).then(function(dbMember) {
+            // console.log(dbMember)
+            // add member information to the page
+            // reload page
+        // })
     // })
-// })
 
 
 
-// remove a member from the book club - this will be triggered by an onclick of a delete button
-// ===========================================================================
-// app.delete('/api/members/:id, function(req, res) {
-    // db.Member.destroy({
-        // where: {
-            // id: req.params.id
-        // }
-    // }).then(function(dbMember)) {
-        // res.json(dbMember)
-    // }
-// }
+    // remove a member from the book club - this will be triggered by an onclick of a delete button
+    // ===========================================================================
+    app.delete('/api/members/:id', function(req, res) {
+        db.Member.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbMember) {
+            res.json(dbMember)
+        });
+    });
 
 }
